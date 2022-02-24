628e34dd885161e9d855ab78eab780bfa9970f55
VixyLeDieu <tvv3show@gmail.com>
2022-02-24 13:33:14 +0100

    ajout du new input system et du script de deplacement

---
+++ Assets/Module 1/Components/PlayerMovements.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovements : MonoBehaviour
+{
+    public bool isGrounded;
+    Vector2 directionVector;
+
+    public float speed = .8f;
+    Rigidbody body;
+
+    public float jumpForce = 1f;
+    public float distToGround = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponent<Rigidbody>();
+        isGrounded = true;
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+
+        directionVector = context.ReadValue<Vector2>();
+    }
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        if (isGrounded && context.performed)
+        {
+            body.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(0, 0, -directionVector.x).normalized;
+        body.MovePosition(transform.position + (movement * speed * Time.deltaTime));
+
+        if (Physics.Raycast(transform.position, Vector3.down, distToGround + 0.1f))
+        {
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
+        }
+    }
+}
